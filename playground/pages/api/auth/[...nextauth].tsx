34527ae84696612a57080363e472576a885c3a11
leona-art <leonagraph@gmail.com>
2023-01-25 00:05:36 +0900

    [update]ログイン機能の実装（Discord,Credentials）

--- playground/pages/api/auth/[...nextauth].tsx
+++ playground/pages/api/auth/[...nextauth].tsx
@@ -8,9 +8,31 @@ import Credentials from 'next-auth/providers/credentials'
 export default NextAuth({
     providers: [
         Discord({
-            clientId: "",
-            clientSecret: ""
+            clientId: process.env.DISCORD_CLIENT_ID as string,
+            clientSecret: process.env.DISCORD_CLIENT_SECRET as string,
         }),
-        
-    ]
+        Credentials({
+            name: "our account",
+            credentials: {
+                username: { label: "Username", type: "text" },
+                password: { label: "Password", type: "password" }
+            },
+            async authorize(credentials, req) {
+                const user = { id: "1", name: "J Smith", email: "jsmith@example.com" }
+                if (
+                    credentials?.username === process.env.USER_NAME
+                    &&
+                    credentials?.password === process.env.PASSWORD
+                ) {
+                    return user
+                } else {
+                    return null
+                }
+            },
+        })
+    ],
+    session: {
+        strategy: "jwt",
+        maxAge: 30 * 24 * 60 * 60
+    }
 });
